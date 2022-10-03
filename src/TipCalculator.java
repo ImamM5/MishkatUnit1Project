294ff4cefd07ec6ad76b27097d88d95280bf3756
ImamM5 <imamm@nycstudents.net>
2022-10-03 08:04:50 -0400

    added comments

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -10,17 +10,17 @@ public class TipCalculator
         System.out.println("-------------------------------");
         System.out.println();
         System.out.print("Please input the total bill: ");
-        double tBill = input.nextDouble();
+        double tBill = input.nextDouble(); //user inputs the total bill amount
         System.out.print("Please input the tip percentage (in a whole number, without the percentage sign): ");
-        double tTip = input.nextDouble();
+        double tTip = input.nextDouble(); //user inputs the percentage amount in whole number
         System.out.print("Please input the number of people in the party: ");
-        int people = input.nextInt();
+        int people = input.nextInt(); //user inputs the total number of people in the party
         System.out.println();
 
-        double tipAmount = tBill*(tTip/100);
-        double tTipBill = tBill + tipAmount;
-        double tipPerPerson = (tBill/people) * (tTip/100);
-        double tPerPerson = (tBill/people) + tipPerPerson;
+        double tipAmount = tBill*(tTip/100); // calculates the tip amount
+        double tTipBill = tBill + tipAmount; // calculates the total bill + tip
+        double tipPerPerson = (tBill/people) * (tTip/100); // calculates tip per person
+        double tPerPerson = (tBill/people) + tipPerPerson; // calculates total per person + tip
 
         System.out.print("Total tip amount: ");
         System.out.printf("%.2f", tipAmount);
